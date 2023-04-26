e21d4b5173815c81603a8ff21324120034b18413
tonible14012002 <namanhble14012002@gmail.com>
2023-04-26 14:12:00 +0700

    add dash board

--- uwc_2.0/views/src/views/Dashboard/index.js
+++ uwc_2.0/views/src/views/Dashboard/index.js
@@ -1,12 +1,89 @@
-
-
+import './style.css'
 
 const Dashboard = () => {
     return (
-        <div className="h-[2000px]">
-        oheoes
+        <>
+            <section>
+</section>
+<aside>
+  <nav className="rad-sidebar">
+    <ul>
+      <li>
+        <a href="#" className="inbox">
+          <i className="fa fa-dashboard"><span className="icon-bg rad-bg-success"></span></i>
+          <span className="rad-sidebar-item">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="fa fa-bar-chart-o">
+						<span className="icon-bg rad-bg-danger"></span>
+					</i>
+          <span className="rad-sidebar-item">Ticket status</span>
+        </a>
+      </li>
+      <li><a href="#" className="snooz"><i className="fa fa-line-chart"><span className="icon-bg rad-bg-primary"></span></i><span className="rad-sidebar-item">Call trends</span></a></li>
+      <li><a href="#" className="done"><i className="fa fa-area-chart"><span className="icon-bg rad-bg-warning"></span></i><span className="rad-sidebar-item">Heat maps</span></a></li>
+      <li><a href="#"><i className="fa fa-wrench"><span className="icon-bg rad-bg-violet"></span></i><span className="rad-sidebar-item">Settings</span></a></li>
+    </ul>
+  </nav>
+</aside>
+<main>
+  <section>
+    <div className="rad-body-wrapper">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-4 col-md-12 col-xs-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Area Chart</h3>
+              </div>
+              <div className="panel-body">
+                <div id="areaChart" className="rad-chart"></div>
+              </div>
+
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-xs-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Line Chart</h3>
+              </div>
+              <div className="panel-body">
+                <div id="lineChart" className="rad-chart"></div>
+              </div>
+
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-xs-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Donut Chart</h3>
+              </div>
+              <div className="panel-body">
+                <div id="donutChart" className="rad-chart"></div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Bar Chart</h3>
+              </div>
+              <div className="panel-body">
+                <div id="barChart" className="rad-chart"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+        </>
     )
 }
 
-export default Dashboard;
+export default Dashboard
--- uwc_2.0/views/src/views/Login/index.js
+++ uwc_2.0/views/src/views/Login/index.js
@@ -43,28 +43,28 @@ export default function Login() {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900 place-items-center bg-login" style={{height: '100vh'}}>
+        <section className="bg-gray-50 -gray-900 place-items-center bg-login" style={{height: '100vh'}}>
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"   style={{width: '550px', marginTop: '140px'}}>
+            <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 -gray-800 -gray-700"   style={{width: '550px', marginTop: '140px'}}>
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white" style={{textAlign:'center'}}>
+                    <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl -white" style={{textAlign:'center'}}>
                         Sign In
                     </h1>
                     <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 -white">Your Username</label>
                             <input
                                 type="text"
                                 name="login_username"
                                 id="userName"
                                 onChange={(e) => {handleChange(e)}}
                                 value = {inputs.login_username || ""}
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -gray-700 -gray-600 -gray-400 -white :ring-blue-500 :border-blue-500"
                                 placeholder="User Name / User Identification" required=""/>
                         </div>
                         <div>
                             <label
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                className="block mb-2 text-sm font-medium text-gray-900 -white">Password</label>
                             <input
                                 type="password"
                                 name="login_password"
@@ -72,29 +72,29 @@ export default function Login() {
                                 placeholder="••••••••"
                                 onChange={(e) => {handleChange(e)}}
                                 value = {inputs.login_password || ""}
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -gray-700 -gray-600 -gray-400 -white :ring-blue-500 :border-blue-500" required=""/>
                         </div>
                         <div className="flex items-center justify-between">
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 -gray-700 -gray-600 :ring-primary-600 -offset-gray-800" required=""/>
                                 </div>
                                 <div className="ml-3 text-sm">
-                                    <label className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                    <label className="text-gray-500 -gray-300">Remember me</label>
                                 </div>
                             </div>
-                            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                            <a href="#" className="text-sm font-medium text-primary-600 hover:underline -primary-500">Forgot password?</a>
                         </div>
                         <button
                             type="submit"
-                            className="w-full text-dark bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            className="w-full text-dark bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center -primary-600 :bg-primary-700 :ring-primary-800"
                             onSubmit={handleSubmit}
                         >
 
                             Sign in
                         </button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                        <p className="text-sm font-light text-gray-500 -gray-400">
+                            Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline -primary-500">Sign up</a>
                         </p>
                     </form>
                 </div>
