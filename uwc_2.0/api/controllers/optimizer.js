fbc398097f5018e2d784664b2f891215a6f4928e
HuyHieu-19 <donguyenhuyhieu@gmail.com>
2023-04-27 20:33:14 +0700

    Final Updating Optimizer

--- uwc_2.0/api/controllers/optimizer.js
+++ uwc_2.0/api/controllers/optimizer.js
@@ -4,7 +4,7 @@ import { MinPriorityQueue } from '@datastructures-js/priority-queue';
 export const OptimizerController = {
     Optimizer: (req, res) => {
         const inf = 10000;
-        const rootDis = [[2500,1200,1500,2100,2100,3200],[2100,2900,3600,2500,1400,3700]]
+        const rootDis = [[2500, 1200, 1500, 2100, 2100, 3200], [2100, 2900, 3600, 2500, 1400, 3700]]
         const distance = [
             [0, 950, 1200, 1600, 1700, 2400],
             [950, 0, 600, 850, 1300, 2200],
@@ -14,49 +14,110 @@ export const OptimizerController = {
             [2400, 2200, 2700, 1500, 850, 0],
         ]
 
-        const PointsInfo = []
-        const MCPs = req.body.MCPs;
+        let PointsInfo = []
+        let Weight = new Array(10).fill(inf);
+        let mark = new Array(10).fill(false);
+        let visited = new Array(10).fill(0);
+        let MCPs = req.body.MCPs;
 
-        const Parent = []
-        const Data = [];
-        var start = 0;
-        var data = '';
-        let idx = 0;
-        for (let point in MCPs) {
+        let Parent = []
+        let Data = [];
+        let start = 0;
+
+        for (let point in MCPs)
         {
             let location = point
             let num = parseInt(location.slice(5));
-            if(point.isAvailable === true)
+            if (MCPs[point].isAvailable === true && start === 0) start = num;
+            PointsInfo.push({ point: MCPs[point], minDist: inf, index: num, namePoint: point})
+        }
+
+        const isEqual = (arr1, arr2) => {
+            return arr1[0] === arr2[0] && arr1[1] === arr2[1];
+        }
+
+                //find bottle neck point => O(n^2)
+        // >= 3 ==> seem as bottle neck
+        let arrayBottleNeck = Array.from({ length: distance.length }, () => []);
+        let modArrayBottleNeck = []
+        let bottleNeckIdx = []
+        const FindBottleNeck = () => {
+            for(let idx = 0; idx < distance.length;idx++)
             {
-                start = num;
+                let row = PointsInfo[idx].index;
+                if(PointsInfo[idx].point.isAvailable === false) {
+                    idx++;
+                    continue;
+                }
+                let minVal = inf;
+                let minIdx = 0;
+                for(let col = 0; col < distance[row].length; col++)
+                {
+                    if(distance[row][col] !== 0 && minVal > distance[row][col])
+                    {
+                        console.log("minVal: ", minVal, distance[row][col])
+                        minVal = distance[row][col];
+                        minIdx = col;
+                    }
+                }
+                console.log("minIdx: ", minIdx);
+                arrayBottleNeck[minIdx].push([minVal, row])
             }
-            PointsInfo.push({ point: MCPs[point], minDist: inf, index: num, namePoint: point, mark: false, visit: 0 })
+            console.log(arrayBottleNeck);
+            arrayBottleNeck.forEach((subArray, index) => {
+                subArray.sort((a,b) => a[0] - b[0])
+                if(subArray.length >= 3) bottleNeckIdx.push(index)
+            })
+            console.log("---------")
+            console.log(arrayBottleNeck);
+            console.log(bottleNeckIdx);
+            modArrayBottleNeck = arrayBottleNeck.map(subarray => subarray.slice(0, 2))
+            console.log("mod: ", modArrayBottleNeck);
         }
-        }
+
+
+        let upperbound = PointsInfo.length + 1;
+
         const MCPsQueue = new MinPriorityQueue((point) => point.minDist);
         const MCPsOrder = new MinPriorityQueue((point) => point.minDist);
 
+
         const minSpanning = (start) => {
             PointsInfo[start].minDist = 0;
-            // Parent[PointsInfo[start].index] = 0
+            Parent[PointsInfo[start].index] = upperbound; //assign to out of array range
+            Weight[PointsInfo[start].index] = 0;
             MCPsOrder.enqueue(PointsInfo[start]);
 
-            while(!MCPsOrder.isEmpty())
-            {
+            while (!MCPsOrder.isEmpty()) {
                 const curPoint = MCPsOrder.pop();
-                if (PointsInfo[curPoint.index].minDist < curPoint.minDist || curPoint.point.isAvailable === false) continue;
-
-                curPoint.mark = true;
+                if (Weight[curPoint.index] < curPoint.minDist) continue;
+                mark[curPoint.index] = true;
 
                 PointsInfo.forEach((adjPoint) => {
                     const gap = distance[curPoint.index][adjPoint.index];
 
-                    if (gap === 0 || adjPoint.mark === true || adjPoint.point.isAvailable === false) return;
+                    if (gap === 0 || mark[adjPoint.index] === true || adjPoint.point.isAvailable === false) {
+                        return;
+                    }
 
-                    if (adjPoint.minDist > gap) {
-                        adjPoint.minDist = gap;
-                        MCPsOrder.enqueue(adjPoint);
-                        Parent[adjPoint.index] = curPoint.index;
+                    console.log("idx: ",curPoint.index, adjPoint.index, Weight[adjPoint.index], gap, Weight[adjPoint.index]> gap)
+                    //if find bottle neck
+                    if (Weight[adjPoint.index] > gap) {
+                        if(bottleNeckIdx.includes(curPoint.index))
+                        {
+                            if(modArrayBottleNeck[curPoint.index].some(subarray => subarray[1] === adjPoint.index))
+                            {
+                                console.log("test")
+                                console.log(modArrayBottleNeck[curPoint.index])
+
+                            }
+                            else return;
+                        }
+                        Weight[adjPoint.index] = gap
+                        let tmpPoint = {...adjPoint}
+                        tmpPoint.minDist = gap;
+                        MCPsOrder.enqueue(tmpPoint);
+                        Parent[tmpPoint.index] = curPoint.index;
                     }
                 })
             }
@@ -64,21 +125,20 @@ export const OptimizerController = {
             let finish = 0;
             let count = new Array(Parent.length).fill(0);
             Parent.forEach((value, index) => {
-                // Data.push([index, value])
                 count[value]++;
                 count[index]++;
             })
-
+            count[start]--;
+            console.log("--------------------")
             count.forEach((node, index) => {
-                if(node === 1 && begin === 0) begin = index;
-                else if(node === 1) finish = index;
-                else if(node !== 1 && node > 0){
+                if(index === upperbound) return;
+                if (node === 1 && begin === 0) begin = index;
+                else if (node === 1) finish = index;
+                else if (node !== 1 && node > 0) {
                     Data.push(index);
                 }
             })
-
-            if(rootDis[0][begin] > rootDis[0][finish])
-            {
+            if (rootDis[0][begin] > rootDis[0][finish]) {
                 let Tmp = begin;
                 begin = finish;
                 finish = Tmp;
@@ -88,97 +148,38 @@ export const OptimizerController = {
             Data.push(finish);
             Data.push('treatment')
         }
-
+        FindBottleNeck();
         minSpanning(start);
         return res.status(200).json(Data);
     }
 
-        // const minPath = (start, end) => {
-        //     PointsInfo[start].minDist = 0;
-        //     Parent[PointsInfo[start].index] = 0
-        //     MCPsQueue.enqueue(PointsInfo[start]);
+    // const minPath = (start, end) => {
+    //     PointsInfo[start].minDist = 0;
+    //     Parent[PointsInfo[start].index] = 0
+    //     MCPsQueue.enqueue(PointsInfo[start]);
 
-        //     while (!MCPsQueue.isEmpty()) {
-        //         const curPoint = MCPsQueue.pop();
-        //         if (curPoint.minDist > PointsInfo[curPoint.index].minDist) continue;
-        //         PointsInfo.forEach((adjPoint) => {
-        //             const gap = distance[curPoint.index][adjPoint.index];
+    //     while (!MCPsQueue.isEmpty()) {
+    //         const curPoint = MCPsQueue.pop();
+    //         if (curPoint.minDist > PointsInfo[curPoint.index].minDist) continue;
+    //         PointsInfo.forEach((adjPoint) => {
+    //             const gap = distance[curPoint.index][adjPoint.index];
 
-        //             if (gap === 0) return;
+    //             if (gap === 0) return;
 
-        //             if (adjPoint.minDist > curPoint.minDist + gap) {
-        //                 adjPoint.minDist = curPoint.minDist + gap;
-        //                 MCPsQueue.enqueue(adjPoint);
-        //                 Parent[adjPoint.index] = curPoint.index;
-        //             }
-        //         })
-        //     }
-        //     Data.push(PointsInfo[end].namePoint)
-        //     while (end != start) {
-        //         Data.push(PointsInfo[Parent[end]].namePoint);
-        //         end = Parent[end];
-        //     }
-        // }
-
+    //             if (adjPoint.minDist > curPoint.minDist + gap) {
+    //                 adjPoint.minDist = curPoint.minDist + gap;
+    //                 MCPsQueue.enqueue(adjPoint);
+    //                 Parent[adjPoint.index] = curPoint.index;
+    //             }
+    //         })
+    //     }
+    //     Data.push(PointsInfo[end].namePoint)
+    //     while (end != start) {
+    //         Data.push(PointsInfo[Parent[end]].namePoint);
+    //         end = Parent[end];
+    //     }
+    // }
 
 }
 
 
-const distance = ({latitude: lat1, longitude: lon1}, {latitude: lat2, longitude: lon2}) => {
-    const R = 6371; // Radius of the Earth in kilometers
-    const disLat = toRadians(lat2 - lat1);
-    const disLon = toRadians(lon2 - lon1);
-    const a =
-        Math.sin(disLat / 2) * Math.sin(disLat / 2) +
-        Math.cos(toRadians(lat1)) *
-        Math.cos(toRadians(lat2)) *
-        Math.sin(disLon / 2) *
-        Math.sin(disLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const result = R * c;
-    console.log(result)
-    return result;
-}
-
-const toRadians = (degrees) => {
-    return degrees * (Math.PI / 180);
-}
-
-/*
-{
-    "MCPs":{
-        "point0": {
-            "longitude": 10.762357,
-            "latitude": 106.662085,
-            "isAvailable": true
-        },
-        "point1": {
-            "longitude": 10.768384,
-            "latitude": 106.658254,
-            "isAvailable": true
-        },
-        "point2": {
-            "longitude": 10.769650,
-            "latitude": 106.670166,
-            "isAvailable": true
-        },
-        "point3": {
-            "longitude": 10.775690,
-            "latitude": 106.667341,
-            "isAvailable": true
-        },
-        "point4": {
-            "longitude": 10.762120,
-            "latitude": 106.668560,
-            "isAvailable": true
-        },
-        "point5": {
-            "longitude": 10.757372,
-            "latitude": 106.663417,
-            "isAvailable": true
-        }
-    },
-    "start": 2,
-    "end": 5
-}
-*/
